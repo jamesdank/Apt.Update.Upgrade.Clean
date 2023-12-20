c2427f1d0cc4c499e87fde75477187b1a41dd936
jamesdank <94645949+jamesdank@users.noreply.github.com>
2023-12-20 02:48:24 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-#####################################################################
-# Sound Files
-UPDATE="/home/$USER/apt.update.upgrade.clean/mp3/update.mp3"
-UPGRADE="/home/$USER/apt.update.upgrade.clean/mp3/upgrade.mp3"
-CLEAN="/home/$USER/apt.update.upgrade.clean/mp3/clean.mp3"
-READY="/home/$USER/apt.update.upgrade.clean/mp3/ready.mp3"
-#####################################################################
+function update () {
+    ##############################################################################
+    # Sound Files
+    update="/home/$USER/Bash.Function.Apt.Update.Upgrade.Clean/mp3/update.mp3"
+    upgrade="/home/$USER/Bash.Function.Apt.Update.Upgrade.Clean/mp3/upgrade.mp3"
+    clean="/home/$USER/Bash.Function.Apt.Update.Upgrade.Clean/mp3/clean.mp3"
+    ready="/home/$USER/Bash.Function.Apt.Update.Upgrade.Clean/mp3/ready.mp3"
+    ##############################################################################
 
-if ! command -v ffplay &> /dev/null
-then
-    echo "ffplay could not be found"
-    exit 0
-fi
+    if [ ! command -v ffplay &> /dev/null ]; then
+        echo "ffplay could not be found"
+        exit 0
+    fi
 
-echo -e "\nUpdating system..." 
-ffplay $UPDATE -autoexit -nodisp 2> /dev/null 
-sleep 2 
-sudo apt update -y 
+    echo -e "\nUpdating system..." 
+    ffplay $update -autoexit -nodisp 2> /dev/null 
+    sleep 2 
+    sudo apt update -y 
 
-echo -e "\nUpgrading system..." 
-ffplay $UPGRADE -autoexit -nodisp 2> /dev/null 
-sleep 2 
-sudo apt upgrade -y 
-        
-echo -e "\ncleaning up update files and purging unused packages..." 
-ffplay $CLEAN -autoexit -nodisp 2> /dev/null 
-sleep 2 
-sudo apt clean -y 
-sudo apt autoremove -y
-ffplay $READY -autoexit -nodisp 2> /dev/null 
+    echo -e "\nUpgrading system..." 
+    ffplay $upgrade -autoexit -nodisp 2> /dev/null 
+    sleep 2 
+    sudo apt upgrade -y 
+            
+    echo -e "\ncleaning up update files and purging unused packages..." 
+    ffplay $clean -autoexit -nodisp 2> /dev/null 
+    sleep 2 
+    sudo apt clean -y 
+    sudo apt autoremove -y
+    ffplay $ready -autoexit -nodisp 2> /dev/null 
+}
